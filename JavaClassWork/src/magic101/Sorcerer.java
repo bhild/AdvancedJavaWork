17bb3fad73fd34d365a37512758f1664c729aeab
bhild <bryanhildebrand1@gmail.com>
2020-08-24 18:16:55 -0700

    filled in non null constructors

--- JavaClassWork/src/magic101/Sorcerer.java
+++ JavaClassWork/src/magic101/Sorcerer.java
@@ -5,7 +5,27 @@ public class Sorcerer {
 	private int knownSpells;
 	private String name;
 	private String element;
-	public Sorcerer() {}
+	public Sorcerer() {
+		isNatural = true;
+		knownSpells = 0;
+		name = "Wanderer";
+		element = "chaos";
+	}
+	public Sorcerer(boolean isNatural, int knownSpells,	String name, String element) {
+		this.isNatural=isNatural;
+		this.knownSpells=knownSpells;
+		this.name=name;
+		this.element=element;
+	}
+	public Sorcerer(String name, String element) {
+		this.name=name;
+		this.element=element;
+	}
+	public Sorcerer(int knownSpells, String name, String element) {
+		this.knownSpells=knownSpells;
+		this.name=name;
+		this.element=element;
+	}
 	public boolean isNatural() {
 		return isNatural;
 	}
