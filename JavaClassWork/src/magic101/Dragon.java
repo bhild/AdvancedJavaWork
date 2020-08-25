5fdca5e0d221f9b362b80dfa5a824657cb7c97d3
bhild <bryanhildebrand1@gmail.com>
2020-08-24 17:51:17 -0700

    added static var and it getter

--- JavaClassWork/src/magic101/Dragon.java
+++ JavaClassWork/src/magic101/Dragon.java
@@ -4,6 +4,7 @@ public class Dragon extends Enemy{
 	//vars
 	private boolean flying;
 	private boolean breathingFire;
+	private static int numLegends = 0;
 	//constructors m
 	public Dragon() {
 		super();
@@ -14,11 +15,13 @@ public class Dragon extends Enemy{
 		super(name,might,isHostile);
 		setFlying(true);
 		setBreathingFire(false);
+		numLegends += (might>1000)?1:0;
 
 	}public Dragon(String name,int might,boolean isHostile,boolean flying,boolean breathingFire) {
 		super(name,might,isHostile);
 		this.flying = flying;
 		this.breathingFire = breathingFire;
+		numLegends+= (might>1000)?1:0;
 
 	}//end full constructor
 	public boolean isFlying() {
@@ -43,5 +46,8 @@ public class Dragon extends Enemy{
 	@Override
 	public String toString() {
 		return super.toString()+"\nisflying: "+flying+"\nisBreathingFire: "+breathingFire+"\n";
+	}
+	public static int getNumLegends() {
+		return numLegends;
 	}
 }
