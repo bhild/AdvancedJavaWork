85f17199b882a59853c7da0a70002f5c89a64ca1
bhild <bryanhildebrand1@gmail.com>
2020-08-24 17:50:13 -0700

    added static vars and the getters

--- JavaClassWork/src/magic101/Slime.java
+++ JavaClassWork/src/magic101/Slime.java
@@ -5,21 +5,29 @@ public class Slime extends Enemy{
 	//
 	private boolean hasForm;
 	private boolean isAcidic;
+	private static int slimeCount = 0;
+	private static int acidCount = 0;
 	//constructors m
 	public Slime() {
 		super();
 		setHasForm(true);
 		setAcidic(false);
+		slimeCount ++;
 	}
 	public Slime(String name,int might,boolean isHostile) {
 
 		super(name,might,isHostile);
 		setHasForm(true);
 		setAcidic(false);
+		slimeCount ++;
+
 	}public Slime(String name,int might,boolean isHostile,boolean hasForm,boolean isAcidic) {
 		super(name,might,isHostile);
 		this.setHasForm(hasForm);
 		this.setAcidic(isAcidic);
+		slimeCount ++;
+		acidCount += (isAcidic)?1:0;
+
 
 	}//end full constructor
 	
@@ -52,5 +60,14 @@ public class Slime extends Enemy{
 	}
 	public boolean isScarry() {
 		return (might>10&&isAcidic||might>20&&!hasForm||!hasForm&&isAcidic);
+	}
+	public static int getSlimeCount() {
+		return slimeCount;
+	}
+	public static int getAcidCount() {
+		return acidCount;
+	}
+	public static double percentAcid() {
+		return 100*acidCount/slimeCount;
 	}
 }
