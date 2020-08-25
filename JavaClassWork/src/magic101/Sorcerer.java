bada58bbc38e0ad789db13cd3e55372f4a8a30d4
bhild <bryanhildebrand1@gmail.com>
2020-08-24 18:12:39 -0700

    added instance vars null constructor and getters and setters

--- JavaClassWork/src/magic101/Sorcerer.java
+++ JavaClassWork/src/magic101/Sorcerer.java
@@ -1,5 +1,34 @@
 package magic101;
 
 public class Sorcerer {
-
+	private boolean isNatural;
+	private int knownSpells;
+	private String name;
+	private String element;
+	public Sorcerer() {}
+	public boolean isNatural() {
+		return isNatural;
+	}
+	public void setNatural(boolean isNatural) {
+		this.isNatural = isNatural;
+	}
+	public int getKnownSpells() {
+		return knownSpells;
+	}
+	public void setKnownSpells(int knownSpells) {
+		this.knownSpells = knownSpells;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getElement() {
+		return element;
+	}
+	public void setElement(String element) {
+		this.element = element;
+	}
+	
 }
